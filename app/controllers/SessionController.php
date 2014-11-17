cc96a9b464bb809b24a357961df483ab850f6bdc
Lynn <homlean@gmail.com>
2014-11-17 23:47:49 +0800

    session controller

---
+++ app/controllers/SessionController.php
@@ -0,0 +1,41 @@
+<?php
+
+class SessionController extends \BaseController {
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /session/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /session
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /session/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
