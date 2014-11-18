544309c1d26bbbaaa3638f67a01a4fb3f4ff9ece
Honglin <Lynn@Honglin.local>
2014-11-18 10:16:57 +0800

    migrate::users

---
+++ app/database/migrations/2014_11_18_021545_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 60);
+			$table->string('password', 100);
+			$table->string('remember_token', 100);
+			$table->string('email', 40)->unique();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
